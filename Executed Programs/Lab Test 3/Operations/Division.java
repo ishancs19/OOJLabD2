b2cef158c0f1ee35194f6ea59f167eab9a9955e5
ishancs19 <71491466+ishancs19@users.noreply.github.com>
2021-01-09 13:11:10 +0530

    Create Division.java

---
+++ Executed Programs/Lab Test 3/Operations/Division.java
@@ -0,0 +1,11 @@
+package Operations;
+import java.util.*;
+import java.lang.*;
+public class Division {
+	double c, d;
+	public Division(double c, double d) {
+		this.c = c;
+		this.d = d;
+		System.out.println( c + "/" + d + "= " + (c/d));
+	}
+}
