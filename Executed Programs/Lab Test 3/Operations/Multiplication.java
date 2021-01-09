f1d803540b4bd7c87c67553ef9e2978517ef49f2
ishancs19 <71491466+ishancs19@users.noreply.github.com>
2021-01-09 13:11:43 +0530

    Create Multiplication.java

---
+++ Executed Programs/Lab Test 3/Operations/Multiplication.java
@@ -0,0 +1,11 @@
+package Operations;
+import java.util.*;
+import java.lang.*;
+public class Multiplication {
+	double c, d;
+	public Multiplication(double c, double d) {
+		this.c = c;
+		this.d = d;
+		System.out.println( c + "*" + d + "= " + (c*d));
+	}
+}
